4826f316ee82b5a2e8bcffbfdc6404aa37c4af3a
jaiderperez <jaidernico408@gmail.com>
2020-08-06 20:42:32 -0500

    Add connection classes

---
+++ src/co/edu/usta/hotel/config/ParametersDB.java
@@ -0,0 +1,12 @@
+package co.edu.usta.hotel.config;
+
+public class ParametersDB {
+
+    public static final String DB_NAME = "db_hotel";
+    public static final String USER_NAME = "user_mysql";
+    public static final String PASSWORD = "123456";
+    public static final String PORT = "3306";
+    public static final String HOST = "localhost";
+    public static final String DRIVER = "mysql";
+
+}
